22b96cffd9c6b9603769b35c93b668f1bf853ba0
Adam A. Gregory <adam@causecast.org>
2011-07-26 13:12:57 -0700

    Added A module file template and an install file template.

--- drupal.ruble/templates/templates.rb
+++ drupal.ruble/templates/templates.rb
@@ -2,10 +2,18 @@ require 'ruble'
 
 template "Module Template" do |t|
   t.filetype = "*.module"
+  t.replace_parameters = true
   t.invoke do |context|
-    ENV['TM_DATE'] = Time.now.strftime("%Y-%m-%d")
-    module = find_module_name(ENV['TM_DIRECTORY'])
-    raw_contents = IO.read("#{ENV['TM_BUNDLE_SUPPORT']}/templates/template.module")
+    raw_contents = IO.read("#{ENV['TM_BUNDLE_SUPPORT']}/../templates/template.module")
+    raw_contents.gsub(/\$\{([^}]*)\}/) {|match| ENV[match[2..-2]] }
+ end
+end
+
+template "Install Template" do |t|
+  t.filetype = "*.install"
+  t.replace_parameters = true
+  t.invoke do |context|
+    raw_contents = IO.read("#{ENV['TM_BUNDLE_SUPPORT']}/../templates/template.install")
     raw_contents.gsub(/\$\{([^}]*)\}/) {|match| ENV[match[2..-2]] }
  end
 end
